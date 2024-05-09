58729e9b6c069849eaff0bec36f6356fa37a88ed
omersuve <omersuve@gmail.com>
2024-05-10 00:10:17 +0300

    slider

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import parse from 'html-react-parser';
 import styles from "./index.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import Slider from '@mui/material/Slider';
 
 
 interface NewsCardProps {
@@ -12,11 +13,31 @@ interface NewsCardProps {
 }
 
 function NewsCard({title, isOpen}: NewsCardProps) {
+    const [vote, setVote] = useState(0);
+
+    function valuetext(value: number) {
+        return `${value}°C`;
+    }
+
     return (
         <div className={`${styles["box"]} shadow`}>
             <h3 className="relative text-black">
                 {parse(title)}
             </h3>
+            <Slider
+                aria-label="Vote"
+                defaultValue={0}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                shiftStep={1}
+                onChange={(event, newValue) => {
+                    setVote(newValue as number)
+                }}
+                step={1}
+                marks
+                min={-3}
+                max={3}
+            />
             <div className={styles["toggle-btn"]}>
                 {isOpen &&
                     <FontAwesomeIcon
