652dd7e8239240c89521a592c8348b296e5dec82
omersuve <omersuve@gmail.com>
2024-06-21 16:58:48 +0300

    debug magiceden scraper api

--- src/pages/api/nft.tsx
+++ src/pages/api/nft.tsx
@@ -18,41 +18,42 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const data: DappRadarNft = await response.json();
 
         const fetchMagicEdenData = async (nftName: string) => {
-            const magicEdenResponse = await fetch(`https://api-mainnet.magiceden.io/v2/unifiedSearch/xchain/collection/${nftName}?edge_cache=true&limit=5`, {
-                headers: {
-                    "accept": "application/json, text/plain, */*",
-                    "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6",
-                    "priority": "u=1, i",
-                    "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
-                    "sec-ch-ua-mobile": "?1",
-                    "sec-ch-ua-platform": "\"Android\"",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-site",
-                    "Referer": "https://magiceden.io/",
-                    "Referrer-Policy": "strict-origin-when-cross-origin",
-                },
-                body: null,
-                method: "GET",
-            });
+            const meUrl = `https://api-mainnet.magiceden.io/v2/unifiedSearch/xchain/collection/${nftName}?edge_cache=true&limit=5`;
+            // const magicEdenResponse = await fetch(`https://api-mainnet.magiceden.io/v2/unifiedSearch/xchain/collection/${nftName}?edge_cache=true&limit=5`, {
+            //     headers: {
+            //         "accept": "application/json, text/plain, */*",
+            //         "accept-language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6",
+            //         "priority": "u=1, i",
+            //         "sec-ch-ua": "\"Google Chrome\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+            //         "sec-ch-ua-mobile": "?1",
+            //         "sec-ch-ua-platform": "\"Android\"",
+            //         "sec-fetch-dest": "empty",
+            //         "sec-fetch-mode": "cors",
+            //         "sec-fetch-site": "same-site",
+            //         "Referer": "https://magiceden.io/",
+            //         "Referrer-Policy": "strict-origin-when-cross-origin",
+            //     },
+            //     body: null,
+            //     method: "GET",
+            // });
 
-            const magicEdenTextData = await magicEdenResponse.text();
-            let magicEdenData;
+            const proxyUrl = `http://api.scraperapi.com?api_key=${process.env.SC_API_KEY}&url=${encodeURIComponent(meUrl)}`;
 
-            try {
-                magicEdenData = JSON.parse(magicEdenTextData);
-            } catch (e) {
-                console.error("Failed to parse MagicEden response as JSON", magicEdenTextData);
-                throw new Error("Invalid MagicEden JSON response");
+            const response = await fetch(proxyUrl);
+            if (!response.ok) {
+                throw new Error(`Request failed with status ${response.status}`);
             }
-
-            return magicEdenData;
+            const textData = await response.text();
+            if (textData.startsWith("<!DOCTYPE html>")) {
+                throw new Error("Received HTML response, likely blocked by Cloudflare");
+            }
+            return JSON.parse(textData);
         };
 
         const promises: Promise<NftData | null>[] = data.results.map(async (nft, index): Promise<NftData | null> => {
             try {
                 if (nft.name == "STEPN") return null;
-                await delay(index * 500);
+                // await delay(index * 100);
                 const magicEdenData = await fetchMagicEdenData(nft.name);
                 return {
                     name: nft.name,
