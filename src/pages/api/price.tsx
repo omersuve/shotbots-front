444f855a4dd0db2505f2a7ceb5b4446bd723a08f
omersuve <omersuve@gmail.com>
2024-07-28 09:15:37 +0300

    price uses vercel python server

--- src/pages/api/price.tsx
+++ src/pages/api/price.tsx
@@ -1,34 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { load } from "cheerio";
-
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        // Check if the request method is POST
-        if (req.method !== "POST") {
-            return res.status(405).json({ error: "Method not allowed. Only POST requests are allowed." });
-        }
-
-        // Get the request body
-        const tickers: string[] = req.body;
-
-        const responseObj: { [key: string]: { price: string; change: string } }  = {};
-
-        for (const t of tickers) {
-            const url = `https://www.coindesk.com/price/${t}`;
-            const response = await fetch(url);
-            const html = await response.text();
-
-            const $ = load(html);
-
-            const price = $(".currency-pricestyles__Price-sc-1v249sx-0").first().text().trim();
-            const change = $(".price-valuestyles__PriceValueWrapper-sc-h5ehzl-0 h6").first().text().trim();
-
-            responseObj[t] = { price, change };
-        }
-
-        res.json({ prices: responseObj });
-    } catch (e) {
-        console.error(e);
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
     }
-}
+
+    const { tickers } = req.body;
+    const response = await fetch("https://vercel-server-production.up.railway.app/api/price", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ tickers }),
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+};
