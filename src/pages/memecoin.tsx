12a9699df2d7fdf7b447554dbfa4f1f9baf3d2e2
omersuve <omersuve@gmail.com>
2024-11-04 21:11:37 +0300

    default memecoin view changed to trending

--- src/pages/memecoin.tsx
+++ src/pages/memecoin.tsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { MemecoinView, TrendingView, TopMarketCapMemecoinsView } from "../views";
+import {
+  MemecoinView,
+  TrendingView,
+  TopMarketCapMemecoinsView,
+} from "../views";
 import TabMeme from "../components/TabMeme";
 
 const Memecoin: NextPage = (props) => {
-    const [selectedTab, setSelectedTab] = useState("memecoin");
+  const [selectedTab, setSelectedTab] = useState("trending");
 
-    return (
-      <div>
-          <Head>
-              <title>Memecoin</title>
-          </Head>
-          <TabMeme selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-          {selectedTab === "memecoin" && <MemecoinView />}
-          {selectedTab === "trending" && <TrendingView />}
-          {selectedTab === "top-market-cap" && <TopMarketCapMemecoinsView />}
-      </div>
-    );
+  return (
+    <div>
+      <Head>
+        <title>Memecoin</title>
+      </Head>
+      <TabMeme selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      {selectedTab === "memecoin" && <MemecoinView />}
+      {selectedTab === "trending" && <TrendingView />}
+      {selectedTab === "top-market-cap" && <TopMarketCapMemecoinsView />}
+    </div>
+  );
 };
 
 export default Memecoin;
