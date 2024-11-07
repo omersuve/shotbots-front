d4fe8861aa2e7e616f1c426901dfe0d7ed1cf98a
omersuve <omersuve@gmail.com>
2024-11-07 18:13:20 +0300

    added docs

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-200 text-gray-800 py-3 opacity-50 pointer-events-none">
+    <footer className="bg-gray-200 text-gray-800 py-3 opacity-50">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="md:mb-0 text-center">
@@ -11,9 +12,15 @@ const Footer: React.FC = () => {
               &copy; {new Date().getFullYear()} Shot Bots. All rights reserved.
             </p>
           </div>
-          <div className="hidden lg:flex lg:space-x-4">
-            <div className="text-sm hover:text-gray-400">Docs</div>
-            <div className="text-sm hover:text-gray-400">Privacy Policy</div>
+          <div className="hidden lg:flex lg:space-x-4 text-base">
+            <div className="hover:text-gray-400">
+              <Link href="https://docs.shotbots.app/" target="_blank">
+                Docs
+              </Link>
+            </div>
+            <div className="pointer-events-none hover:text-gray-400">
+              Privacy Policy
+            </div>
           </div>
         </div>
       </div>
--- src/components/MainBody/index.tsx
+++ src/components/MainBody/index.tsx
@@ -250,7 +250,7 @@ const MainBody = ({ gradient, message, message2 }: any) => {
           </>
         )}
 
-        <div className="flex items-center justify-center p-4 mt-2 opacity-75">
+        <div className="flex items-center justify-center p-4 mt-2 opacity-75 gap-4">
           <Link href="https://x.com/theshotbots" passHref target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -258,6 +258,15 @@ const MainBody = ({ gradient, message, message2 }: any) => {
               className={styles["x-icon"]}
             >
               <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+            </svg>
+          </Link>
+          <Link href="https://docs.shotbots.app/" passHref target="_blank">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              className={styles["x-icon"]}
+            >
+              <path d="M96 0C43 0 0 43 0 96L0 416c0 53 43 96 96 96l288 0 32 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l0-64c17.7 0 32-14.3 32-32l0-320c0-17.7-14.3-32-32-32L384 0 96 0zm0 384l256 0 0 64L96 448c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16zm16 48l192 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-192 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
             </svg>
           </Link>
         </div>
